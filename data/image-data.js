b787724c909e47969c5f9b3ef79a7f31825df0ea
Oluwatoyosi <oladipooluwatoyosi@yahoo.com>
2023-03-07 14:02:51 +0100

    initial commit

---
+++ data/image-data.js
@@ -0,0 +1,12 @@
+export default [
+  "/images/pexels-anastasia-11041446.jpg",
+  "/images/pexels-anthony-rodriguez-3416864.jpg",
+  "/images/pexels-beyzaa-yurtkuran-15102251.jpg",
+  "/images/pexels-engin-akyurt-2347302.jpg",
+  "/images/pexels-joaquin-carfagna-15550234.jpg",
+  "/images/pexels-lisa-fotios-1655329.jpg",
+  "/images/pexels-musa-ortaç-3185222.jpg",
+  "/images/pexels-pragyan-bezbaruah-1844228.jpg",
+  "/images/pexels-şeyda-nur-uğur-8340925.jpg",
+  "/images/pexels-shrinish-donde-11118689.jpg",
+];
